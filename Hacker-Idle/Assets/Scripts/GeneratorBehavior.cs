2ae02f7e7de6b19bbac757dacfcfaee0afcfaa9b
Nikita Lampadov <lampadovnikita@gmail.com>
2020-08-02 23:03:45 +0700

    Add script for a generator data controll

---
+++ Hacker-Idle/Assets/Scripts/GeneratorBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GeneratorBehavior : MonoBehaviour
+{
+	public delegate void Produce(float producedNum);
+	public event Produce OnProduce;
+
+	[SerializeField]
+	private GeneratorData data = default;
+
+	private void Start()
+	{
+		if (data.Level > 0)
+		{
+			StartCoroutine(ProduceLoopCoroutine());
+		}
+	}
+
+	public void Upgrade()
+	{
+		data.Level++;
+
+		data.UpgradeCost *= data.UpgradeCostGrowthRate;
+
+		if (data.Level > 1)
+		{
+			data.ProductionRate += data.BaseProductionRate * data.ProductionMultiplier;
+		}
+		else if (data.Level == 1)
+		{
+			StartCoroutine(ProduceLoopCoroutine());
+		}
+	}
+
+	private IEnumerator ProduceLoopCoroutine()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(1f);
+
+			OnProduce.Invoke(data.ProductionRate);
+		}
+	}
+}
