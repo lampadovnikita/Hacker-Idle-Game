0c9e71772f63cfc6732044d72b7c4f4af4c94e07
Nikita Lampadov <lampadovnikita@gmail.com>
2020-08-03 22:25:55 +0700

    Add validator script

---
+++ Hacker-Idle/Assets/Scripts/Validator.cs
@@ -0,0 +1,18 @@
+﻿public static class Validator
+{
+	public static void ValidateNonNegative(ref float value)
+	{
+		if (value < 0)
+		{
+			value = 0f;
+		}
+	}
+
+	public static void ValidateNonNegative(ref int value)
+	{
+		if (value < 0)
+		{
+			value = 0;
+		}
+	}
+}
