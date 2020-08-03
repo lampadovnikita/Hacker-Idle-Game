b7be65a1a69a1d16bb610b6a67f39b309273ca4d
Nikita Lampadov <lampadovnikita@gmail.com>
2020-08-03 15:59:27 +0700

    Add generator UI script

---
+++ Hacker-Idle/Assets/Scripts/GeneratorUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorUI : MonoBehaviour
+{
+	[SerializeField]
+	private Generator generator = default;
+
+	public void OnUpgradeButtonPressed()
+	{
+		generator.Upgrade();
+	}
+}
