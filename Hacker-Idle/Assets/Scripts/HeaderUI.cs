9ee705985be42b5d595a986eff4512f2b0c975d5
Nikita Lampadov <lampadovnikita@gmail.com>
2020-08-04 14:12:10 +0700

    Add header UI script

---
+++ Hacker-Idle/Assets/Scripts/HeaderUI.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class HeaderUI : MonoBehaviour
+{
+	[SerializeField]
+	private TextMeshProUGUI currencyAmountUGUI = default;
+
+	private CurrencyAmount currencyAmount;
+
+	private void Start()
+	{
+		currencyAmount = CurrencyAmount.Instance;
+		currencyAmount.OnAmountChanged += OnCurrencyAmountChanged;
+
+		UpdateAmountUI();
+	}
+
+	private void OnCurrencyAmountChanged()
+	{
+		UpdateAmountUI();
+	}
+
+	private void UpdateAmountUI()
+	{
+		currencyAmountUGUI.text = currencyAmount.Amount.ToString();
+	}
+}
