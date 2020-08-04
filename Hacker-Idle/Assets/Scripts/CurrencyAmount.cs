2cca74715189183f3f6f34444833500ac2e33283
Nikita Lampadov <lampadovnikita@gmail.com>
2020-08-04 14:11:53 +0700

    Add main currency script

---
+++ Hacker-Idle/Assets/Scripts/CurrencyAmount.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrencyAmount : MonoBehaviour
+{
+	public static CurrencyAmount Instance { get; private set; }
+
+	public delegate void AmountChanged();
+	public event AmountChanged OnAmountChanged;
+
+	[SerializeField]
+	private List<Generator> generators = default;
+
+	private float amount;
+
+	public float Amount => amount;
+
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+
+		DontDestroyOnLoad(gameObject);
+	}
+
+	private void Start()
+	{
+		foreach (Generator generator in generators)
+		{
+			generator.OnProduced += OnGeneratorProduced;
+		}
+	}
+
+	public void OnGeneratorProduced(float producedAmount)
+	{
+		amount += producedAmount;
+
+		OnAmountChanged?.Invoke();
+	}
+}
