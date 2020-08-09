348d95b3f3ad459ec6c0498b7e7ada63100cd924
Nikita Lampadov <lampadovnikita@gmail.com>
2020-08-09 23:02:38 +0700

    Remove redundant commented code

--- Hacker-Idle/Assets/Scripts/Money/Money.cs
+++ Hacker-Idle/Assets/Scripts/Money/Money.cs
@@ -3,12 +3,6 @@ using UnityEngine;
 
 public abstract class Money<T> : MonoBehaviour where T : IComparable<T>
 {
-
-	//public static CurrencyPurse<T> Instance { get; private set; }
-
-	//[SerializeField]
-	//private List<Generator> generators = default;
-
 	public delegate void AmountChanged();
 	public event AmountChanged OnAmountChanged;
 
@@ -25,33 +19,6 @@ public abstract class Money<T> : MonoBehaviour where T : IComparable<T>
 	private void Awake()
 	{
 		amount = initialAmount;
-	}
-
-	//private void Awake()
-	//{
-	//	if (Instance == null)
-	//	{
-	//		Instance = this;
-	//	}
-	//	else
-	//	{
-	//		Destroy(gameObject);
-	//	}
-
-	//	DontDestroyOnLoad(gameObject);
-	//}
-
-	//private void Start()
-	//{
-	//	foreach (Generator generator in generators)
-	//	{
-	//		generator.OnProduced += OnGeneratorProduced;
-	//	}
-	//}
-
-	public static string GetFormattedAmount(float amount)
-	{
-		return "lollll";
 	}
 
 	public bool AttemptWriteOff(T writeOffAmount)
@@ -84,23 +51,4 @@ public abstract class Money<T> : MonoBehaviour where T : IComparable<T>
 	protected abstract T Add(T lhs, T rhs);
 
 	protected abstract T Subtract(T lhs, T rhs);
-
-	//public bool CanWriteOff(T amountToWriteOff)
-	//{
-	//	if (amount - amountToWriteOff > 0f)
-	//	{
-	//		return true;
-	//	}
-	//	else
-	//	{
-	//		return false;
-	//	}
-	//}
-
-	//private void OnGeneratorProduced(T producedAmount)
-	//{
-	//	amount += producedAmount;
-
-	//	OnAmountChanged?.Invoke();
-	//}
 }
