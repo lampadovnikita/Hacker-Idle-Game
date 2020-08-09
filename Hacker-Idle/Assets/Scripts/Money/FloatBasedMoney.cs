970c7085c3cdd7ea6469fd0a0f4256de9f266d12
Nikita Lampadov <lampadovnikita@gmail.com>
2020-08-09 23:01:04 +0700

    Add ToString methods to get money string representation

--- Hacker-Idle/Assets/Scripts/Money/FloatBasedMoney.cs
+++ Hacker-Idle/Assets/Scripts/Money/FloatBasedMoney.cs
@@ -1,36 +1,68 @@
 ﻿using System;
+using UnityEngine;
 
 public class FloatBasedMoney : Money<float>
 {
-    protected override float Add(float lhs, float rhs)
-    {
-        float res;
+	private static readonly string[] METRIC_PREFIXES = { "", "K", "M", "G", "T", "P", "E", "Z", "Y" };
+	private static readonly int EXP_PER_METRIC = 3;
 
-        try
-        {
-            res = checked(lhs + rhs);
-        }
-        catch (OverflowException e)
-        {
-            res = float.MaxValue;
-        }
+	public static string ToString(float amount)
+	{
+		int exp = Mathf.FloorToInt(Mathf.Log10(Mathf.Abs(amount)));
 
-        return res;
-    }
+		string prefix = "";
 
-    protected override float Subtract(float lhs, float rhs)
-    {
-        float res;
+		if (exp >= 0)
+		{
+			if (exp < METRIC_PREFIXES.Length * EXP_PER_METRIC)
+			{
+				prefix = METRIC_PREFIXES[exp / EXP_PER_METRIC];
+			}
+			else
+			{
+				exp = (METRIC_PREFIXES.Length - 1) * EXP_PER_METRIC;
+				prefix = METRIC_PREFIXES[METRIC_PREFIXES.Length - 1];
+			}
+			amount /= Mathf.Pow(10f, exp);
+		}
 
-        try
-        {
-            res = checked(lhs - rhs);
-        }
-        catch (OverflowException e)
-        {
-            res = float.MinValue;
-        }
+		return string.Format("{0:F5}{1}", amount, prefix);
+	}
 
-        return res;
-    }
+	public override string ToString()
+	{
+		return ToString(Amount);
+	}
+
+	protected override float Add(float lhs, float rhs)
+	{
+		float res;
+
+		try
+		{
+			res = checked(lhs + rhs);
+		}
+		catch (OverflowException e)
+		{
+			res = float.MaxValue;
+		}
+
+		return res;
+	}
+
+	protected override float Subtract(float lhs, float rhs)
+	{
+		float res;
+
+		try
+		{
+			res = checked(lhs - rhs);
+		}
+		catch (OverflowException e)
+		{
+			res = float.MinValue;
+		}
+
+		return res;
+	}
 }
