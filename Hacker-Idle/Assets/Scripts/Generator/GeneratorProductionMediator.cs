587798f277e7b8037fb913c08bd19c24ca8f97e7
Nikita Lampadov <lampadovnikita@gmail.com>
2020-08-11 16:31:50 +0700

    Add GeneratorProductionMediator script to controll flow of production

---
+++ Hacker-Idle/Assets/Scripts/Generator/GeneratorProductionMediator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorProductionMediator : MonoBehaviour
+{
+	[SerializeField]
+	private FloatBasedMoney moneyPurse = default;
+
+	[SerializeField]
+	private List<Generator> generators = default;
+
+	private void Awake()
+	{
+		foreach (Generator g in generators)
+		{
+			g.OnProduced += OnGeneratorProduced;
+		}
+	}
+
+	private void OnGeneratorProduced(float prodecedAmount)
+	{
+		moneyPurse.Deposit(prodecedAmount);
+	}
+}
