03bcf70702af7a25cc870475e47b2596a74d1b19
Nikita Lampadov <lampadovnikita@gmail.com>
2020-08-15 21:51:13 +0700

    Add Player script

---
+++ Hacker-Idle/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public static Player Instance { get; private set; }
+
+	[SerializeField]
+	private FloatBasedResource flopCoinPurse = default;
+
+	[SerializeField]
+	private FloatBasedResource informationPurse = default;
+
+	#region Properties
+	public FloatBasedResource FlopCoinPurse => flopCoinPurse;
+
+	public FloatBasedResource InformationPurse => informationPurse;
+	#endregion
+
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		DontDestroyOnLoad(gameObject);
+	}
+}
