514435719848461b1fc093680a758a48bbe69b33
Nikita Lampadov <lampadovnikita@gmail.com>
2020-08-19 22:53:47 +0700

    Add simple tab system

---
+++ Hacker-Idle/Assets/Scripts/UI/TabButton.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+	public UnityEvent OnTabSelected;
+	public UnityEvent OnTabDeselected;
+	
+	public TabGroup tabGroup;
+
+	private Image backgroundImage;
+
+
+	public Image BackgroundImage => backgroundImage;
+
+	void Start()
+	{
+		backgroundImage = GetComponent<Image>();
+
+		tabGroup.Subscribe(this);
+	}
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		tabGroup.OnTabSelected(this);
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		tabGroup.OnTabEnter(this);
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		tabGroup.OnTabExit(this);
+	}
+
+	public void Select()
+	{
+		OnTabSelected?.Invoke();
+	}
+
+	public void Deselect()
+	{
+		OnTabDeselected?.Invoke();
+	}
+}
---
+++ Hacker-Idle/Assets/Scripts/UI/TabGroup.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TabGroup : MonoBehaviour
+{
+	[SerializeField]
+	private Sprite idleTabSprite = default;
+
+	[SerializeField]
+	private Sprite hoverTabSprite = default;
+
+	[SerializeField]
+	private Sprite activeTabSprite = default;
+
+	[SerializeField]
+	private List<GameObject> objectsToSwap = default;
+
+	private List<TabButton> tabButtons;
+
+	private TabButton selectedTab;
+
+	private void Awake()
+	{
+		tabButtons = new List<TabButton>();
+	}
+
+	public void Subscribe(TabButton tab)
+	{
+		tabButtons.Add(tab);
+
+		if (tab.transform.GetSiblingIndex() == 0)
+		{
+			OnTabSelected(tab);
+		}
+		else
+		{
+			tab.BackgroundImage.sprite = idleTabSprite;
+		}
+	}
+
+	public void OnTabEnter(TabButton tab)
+	{
+		ResetTabs();
+
+		if (tab != selectedTab)
+		{
+			tab.BackgroundImage.sprite = hoverTabSprite;
+		}
+	}
+
+	public void OnTabExit(TabButton tab)
+	{
+		ResetTabs();
+	}
+
+	public void OnTabSelected(TabButton tab)
+	{
+		if (selectedTab != null)
+		{
+			selectedTab.Deselect();
+		}
+
+		selectedTab = tab;
+		selectedTab.Select();
+
+		ResetTabs();
+
+		tab.BackgroundImage.sprite = activeTabSprite;
+
+		int index = tab.transform.GetSiblingIndex();
+		for (int i = 0; i < objectsToSwap.Count; i++)
+		{
+			if (i == index)
+			{
+				objectsToSwap[i].SetActive(true);
+			}
+			else
+			{
+				objectsToSwap[i].SetActive(false);
+			}
+		}
+	}
+
+	private void ResetTabs()
+	{
+		foreach (TabButton tab in tabButtons)
+		{
+			if (tab == selectedTab)
+			{
+				continue;
+			}
+
+			tab.BackgroundImage.sprite = idleTabSprite;
+		}
+	}
+}
