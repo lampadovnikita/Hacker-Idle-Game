ab4d0381eb1a111b54ea140f06b67ad83c0c03a5
Nikita Lampadov <lampadovnikita@gmail.com>
2020-08-29 00:39:45 +0700

    Add trade prototype controller

---
+++ Hacker-Idle/Assets/Scripts/ViewControllers/TradeViewController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class TradeViewController : MonoBehaviour
+{
+    [SerializeField]
+    private TradeView tradeView = default;
+
+	[SerializeField]
+	private float sellPrice = default;
+
+	[SerializeField]
+	private float buyPrice = default;
+
+	private FloatAccumulator sellAccumulator;
+	private FloatAccumulator buyAccumulator;
+
+	private void Start()
+	{
+		sellAccumulator = Player.Instance.InformationAccumulator;
+
+		buyAccumulator = Player.Instance.FlopcoinAccumulator;
+
+		sellAccumulator.OnAmountChanged += (object sender) => UpdateTradeButtonInteractability();
+		
+		
+		tradeView.SetBuyPriceText(buyPrice.ToString());
+		tradeView.SetSellPriceText(sellPrice.ToString());
+	}
+
+	public void OnTradeButtonPressed()
+	{
+		bool isWrittedOff = sellAccumulator.AttemptWriteOff(sellPrice);
+
+		if (isWrittedOff)
+		{
+			buyAccumulator.Deposit(buyPrice);
+		}
+	}
+
+	private void UpdateTradeButtonInteractability()
+	{
+		if (sellAccumulator.Amount >= sellPrice)
+		{
+			tradeView.SetTradeButtonInteractability(true);
+		}
+		else 
+		{
+			tradeView.SetTradeButtonInteractability(false);
+		}
+	}
+}
---
+++ Hacker-Idle/Assets/Scripts/Views/TradeView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TradeView : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI sellPriceUGUI = default;
+
+    [SerializeField]
+    private TextMeshProUGUI buyPriceUGUI = default;
+
+    [SerializeField]
+    private Button tradeButton = default;
+
+    public void SetSellPriceText(string text)
+    {
+        sellPriceUGUI.text = text;
+    }
+
+    public void SetBuyPriceText(string text)
+    {
+        buyPriceUGUI.text = text;
+    }
+
+    public void SetTradeButtonInteractability(bool isInteractable)
+    {
+        tradeButton.interactable = isInteractable;
+    }
+}
