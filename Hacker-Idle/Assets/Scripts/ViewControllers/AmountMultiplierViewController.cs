2c427961bd71293b40acdecf817b195f66a46c8f
Nikita Lampadov <lampadovnikita@gmail.com>
2020-09-08 13:52:23 +0700

    Add amount multiplier

---
+++ Hacker-Idle/Assets/Scripts/ViewControllers/AmountMultiplierViewController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmountMultiplierViewController : MonoBehaviour
+{
+	private static string MULTIPLIER_TEXT_PREFIX = "x";
+
+	public delegate void AmountMultiplierChnaged(object sender, int newMultiplier);
+	public event AmountMultiplierChnaged OnAmountMultiplierChanged;
+
+	[SerializeField]
+	private AmountMultiplierView amountMultiplierView = default;
+
+	[SerializeField]
+	private List<int> amountMultipliers = default;
+
+	private int currentMultiplierIndex;
+
+	private void Awake()
+	{
+		currentMultiplierIndex = 0;
+
+		if (amountMultipliers.Count == 0)
+		{
+			Debug.LogError("Empty multipliers list!");
+		}
+	}
+
+	private void Start()
+	{
+		amountMultiplierView.OnAmountMultiplierButtonClicked += (object sender) => ChangeAmountMultiplier();
+	}
+
+	private void ChangeAmountMultiplier()
+	{
+		if (currentMultiplierIndex >= (amountMultipliers.Count - 1))
+		{
+			currentMultiplierIndex = 0;
+		}
+		else
+		{
+			currentMultiplierIndex++;
+		}
+
+		int newMultiplier = amountMultipliers[currentMultiplierIndex];
+		string amountMultiplierText = MULTIPLIER_TEXT_PREFIX + newMultiplier;
+		amountMultiplierView.SetAmountMultiplierText(amountMultiplierText);
+
+		OnAmountMultiplierChanged?.Invoke(this, newMultiplier);
+	}
+}
---
+++ Hacker-Idle/Assets/Scripts/Views/AmountMultiplierView.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmountMultiplierView : MonoBehaviour
+{
+	public delegate void AmountMultiplierButtonClicked(object sender);
+	public event AmountMultiplierButtonClicked OnAmountMultiplierButtonClicked;
+
+	[SerializeField]
+	private Button amountMultiplierButton = default;
+
+	[SerializeField]
+	private TextMeshProUGUI amountMultiplierUGUI = default;
+
+	private void Awake()
+	{
+		amountMultiplierButton.onClick.AddListener
+		(
+			() =>
+			{
+				OnAmountMultiplierButtonClicked?.Invoke(this);
+			}
+		);
+	}
+
+	public void SetAmountMultiplierText(string text)
+	{
+		amountMultiplierUGUI.text = text;
+	}
+}
