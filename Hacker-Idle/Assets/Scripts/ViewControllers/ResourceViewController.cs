aa4bebe997fd077f60b4ebdf2356ad71b056bcfb
Nikita Lampadov <lampadovnikita@gmail.com>
2020-10-22 00:15:50 +0700

    Add resource view controller

---
+++ Hacker-Idle/Assets/Scripts/ViewControllers/ResourceViewController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ResourceViewController : MonoBehaviour
+{
+    [SerializeField]
+    private ResourceView resourceView = default;
+
+    [SerializeField]
+    private FloatResourceCode resourceCode = default;
+
+    private Resource<float, FloatAccumulator> resource;
+
+	void Start()
+    {
+        resource = Player.Instance.GetResource(resourceCode);
+
+        resource.Accumulator.OnAmountChanged +=
+            (object sender) => UpdateResourceAmountText();
+
+        resourceView.SetIconSprite(resource.BaseData.Icon);
+
+        // Initial setup
+        UpdateResourceAmountText();
+    }
+
+    private void UpdateResourceAmountText()
+    {
+        resourceView.SetAmountText(FloatAccumulator.ToString(resource.Accumulator.Amount));
+    }
+}
